0ac5eaa004445875be11ba97ae84575868232df9
Joshua Reisbord <joshuareisbord@gmail.com>
2021-11-18 12:04:57 -0500

    Stripped down version of pac3man
    source from https://github.com/jspacco/pac3man

---
+++ src/front_end/run.py
@@ -0,0 +1,10 @@
+
+import pacman
+
+
+#pacman.main('pacman.py -p PacmanQAgent -x 2000 -n 2010 -l smallGrid')
+
+# pacman.main('pacman.py --help')
+
+pacman.main('pacman.py --layout=smallClassic -k 1')
+
