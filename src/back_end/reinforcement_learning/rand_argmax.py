c19e5a3186a8a60e75c4553cb91b5affafa32181
Joshua Reisbord <joshuareisbord@gmail.com>
2021-11-25 14:42:39 -0500

    Update rand_argmax.py
    returns a tuple of index from original list, and element found by rand_argmax

--- src/back_end/reinforcement_learning/rand_argmax.py
+++ src/back_end/reinforcement_learning/rand_argmax.py
@@ -1,4 +1,5 @@
 import numpy as np
+import random
 
 def rand_argmax(arr):
     """
@@ -11,10 +12,26 @@ def rand_argmax(arr):
     Returns: 
         returns the index of the highest 
     """
-    max_val = np.max(arr)
-    maxed_elems = []
-    for i in range(len(arr)):
-        if (arr[i] == max_val):
-            maxed_elems.append(i)
-    rand = np.random.randint(len(maxed_elems))
-    return maxed_elems[rand]
+
+    # find the index of the highest value
+    max_val = float('-inf')
+    max_elems = []
+
+    for elem in enumerate(arr):
+        curr = elem[1]
+        idx = elem[0]
+        if curr > max_val: # if elem is bigger then max value, then replace max value and list
+            max_val = curr
+            max_elems = [elem]
+        elif curr == max_val: # if equal to max, append to list of max elements
+            max_elems.append(elem)
+
+    if len(max_elems) == 1: # only one element then it is the max
+        return max_elems[0]
+    
+    return random.choice(max_elems) # (index, value)
+
+if __name__ == "__main__":
+    directions = ['north', 'south', 'east', 'west']
+    lst = [0, 0, 1, 1]
+    print(directions[rand_argmax(lst)[0]])
