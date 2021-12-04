0de0a317e02e26ffacdf580c91a89068dd276ca7
Joshua Reisbord <joshuareisbord@gmail.com>
2021-12-04 18:26:45 -0500

    removed all bulky test run files and changed default paramaters backt o SARSA

--- src/main.py
+++ src/main.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
 
                    # [layout, episodes, ghosts, frame_time, method, verbose, multithreaded]
     # default_params = ['smallClassicTest2', 10, 1, 0.001, 'QLearning', True, False]
-    default_params = ['smallClassicTest2', 5, 1, 0.1, 'FollowQ', False, False]
+    default_params = ['smallClassicTest2', 10, 1, 0.01, 'SARSA', False, False]
 
     core_count = 1 # default core ocunt
 
